5f97b835e2bc23c383870c700ee458b4008a163f
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-05-09 15:09:01 +0300

    Проект Mesto

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,8 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closePopupButton = document.querySelector('.popup__close-button');
-let pop = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+const pop = document.querySelector('.popup');
 
 function popUp() {
-    
     if (pop.classList.contains('popup_closed') === true) {
         pop.classList.remove('popup_closed');
     } else {
@@ -13,3 +12,20 @@ function popUp() {
 
 editButton.addEventListener("click", popUp, false);
 closePopupButton.addEventListener("click", popUp, false);
+const formElement = document.querySelector('.popup__form');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    const nameInput = document.querySelector('.popup__name');
+    const jobInput = document.querySelector('.popup__about');
+    const name = nameInput.value;
+    const job = jobInput.value;
+    const elName = document.querySelector('.profile__info-name');
+    const elJob = document.querySelector('.profile__info-job');
+    elName.textContent = name;
+    elJob.textContent = job;
+    popUp();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
