a360a5201d79866b912c41d8864107f59cf42569
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-05-15 02:43:36 +0300

    hotfix/review1-js

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,29 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close-button');
 const pop = document.querySelector('.popup');
-
-function popUp() {pop.classList.add('popup_opened')}
-function popDown() {pop.classList.remove('popup_opened')}
-
-editButton.addEventListener("click", popUp, false);
-closePopupButton.addEventListener("click", popDown, false);
 const formElement = document.querySelector('.popup__form');
-
 const nameInput = document.querySelector('.popup__name');
 const jobInput = document.querySelector('.popup__about');
 const elName = document.querySelector('.profile__info-name');
 const elJob = document.querySelector('.profile__info-job');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    const name = nameInput.value;
-    const job = jobInput.value;
-    elName.textContent = name;
-    elJob.textContent = job;
-    popDown();
+function popUp() {
+    pop.classList.add('popup_opened');
+    nameInput.value = elName.textContent;
+    jobInput.value = elJob.textContent;
 }
 
+function popDown() {
+    pop.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    elName.textContent = nameInput.value;
+    elJob.textContent = jobInput.value;
+    popDown()
+}
+
+editButton.addEventListener("click", popUp, false);
+closePopupButton.addEventListener("click", popDown, false);
 formElement.addEventListener('submit', formSubmitHandler);
